d113247000d1f26ffbeac252f1ab1c8edc199fbd
Yuvraj004 <yuvrajchat@gmail.com>
2024-05-11 04:28:59 +0530

    Loader1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [quote, setQuote] = useState({});
+
+  const getRandomQuote = () => {
+    setLoading(true);
+    fetch('https://api.quotable.io/random')
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setQuote(data);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {loading ? (
+        <div className="loader-container">
+            <div className="spinner"></div>
+        </div>
+      ) : (
+        <div className="main-content">
+          <h1>Hello World!</h1>
+          <p>
+            This is a demo Project to show how to add animated loading with
+            React.
+          </p>
+          <div className="buttons">
+            <button className="btn">
+              <a href="/">Read Article</a>
+            </button>
+            <button className="btn get-quote" onClick={getRandomQuote}>
+              Generate Quote
+            </button>
+          </div>
+          <div className="quote-section">
+            <blockquote className="quote">{quote.content}</blockquote>-{' '}
+            <span className="author">{quote.author}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
