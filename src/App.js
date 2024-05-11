d3235d238b961988f154d760e71558ac3a134197
Yuvraj004 <yuvrajchat@gmail.com>
2024-05-12 04:56:48 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
                 <a class="nav-link active" aria-current="page" href="/circleLoader">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/loader2">Loader 2</a>
+                <a class="nav-link" href="/loader2">Spinning Cube</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
--- src/Components/Three.js
+++ src/Components/Three.js
@@ -5,23 +5,26 @@ import { useEffect, useRef } from "react";
 function MyThree() {
   const refContainer = useRef(null);
   useEffect(() => {
-    // === THREE.JS CODE START ===
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // document.body.appendChild( renderer.domElement );
-    // use ref as a mount point of the Three.js scene instead of the document.body
+    var renderer = new THREE.WebGLRenderer({ alpha: true }); // Enable scene transparency
+    renderer.setSize(window.innerWidth*0.5, window.innerHeight*0.5);
     refContainer.current && refContainer.current.appendChild( renderer.domElement );
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+    // Create a material with partial opacity
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, opacity: 0.5 });
+
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     camera.position.z = 5;
+
+    renderer.setClearColor(0xcccccc, 0.3); 
+
     var animate = function () {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      cube.rotation.x += 0.02;
+      cube.rotation.y += 0.02;
       renderer.render(scene, camera);
     };
     animate();
@@ -32,4 +35,4 @@ function MyThree() {
   );
 }
 
-export default MyThree
+export default MyThree;
